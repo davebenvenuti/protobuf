7b98cee86dd56b255ce3fa5ef4303876c1a87fe4
Dave Benvenuti <davebenvenuti@gmail.com>
2025-02-27 00:17:32 +0000

    Benchmark a handful of options

--- ruby/bench/bench.rb
+++ ruby/bench/bench.rb
@@ -4,20 +4,76 @@ require 'google/protobuf'
 require 'benchmark/ips'
 require_relative 'message_pb'
 
-def build_message
-  BenchMessage.new(field_1: 123, field_2: "howdy")
+module Kwargs
+  BenchMessage = ::BenchMessage.clone
+
+  class BenchMessage
+    def initialize(**kwargs)
+      init_arena
+
+      kwargs.each_pair { |key, val| init_kwarg(key, val) }
+    end
+  end
 end
 
-rubber_duck_msg = build_message
+module InitKwarg
+  BenchMessage = ::BenchMessage.clone
 
-raise "field_1 mismatch: #{rubber_duck_msg.field_1}" unless rubber_duck_msg.field_1 == 123
-raise "field_2 mismatch: #{rubber_duck_msg.field_2}" unless rubber_duck_msg.field_2 == "howdy"
+  class BenchMessage
+    def initialize(field_1:, field_2:)
+      init_arena
 
+      init_kwarg("field_1", field_1)
+      init_kwarg("field_2", field_2)
+    end
+  end
+end
+
+module UseSetters
+  BenchMessage = ::BenchMessage.clone
+
+  class BenchMessage
+    def initialize(field_1:, field_2:)
+      init_arena
+
+      self.field_1 = field_1
+      self.field_2 = field_2
+    end
+  end
+end
+
+module Positional
+  BenchMessage = ::BenchMessage.clone
+
+  class BenchMessage
+    def initialize(field_1, field_2)
+      init_arena
+
+      init_kwarg("field_1", field_1)
+      init_kwarg("field_2", field_2)
+    end
+  end
+end
+
+
+field_1 = 123
+field_2 = "howdy"
+
+rubber_duck_msgs = [
+  Kwargs::BenchMessage.new(field_1:, field_2:),
+  InitKwarg::BenchMessage.new(field_1:, field_2:),
+  UseSetters::BenchMessage.new(field_1:, field_2:),
+  Positional::BenchMessage.new(field_1, field_2)
+]
+
+rubber_duck_msgs.each_with_index do |rubber_duck_msg, i|
+  raise "[#{i}] field_1 mismatch: #{rubber_duck_msg.field_1}" unless rubber_duck_msg.field_1 == field_1
+  raise "[#{i}] field_2 mismatch: #{rubber_duck_msg.field_2}" unless rubber_duck_msg.field_2 == field_2
+end
 
 Benchmark.ips do |x|
-  x.report("initialize") { build_message }
+  x.report("initialize kwargs.each_pair") { Kwargs::BenchMessage.new(field_1:, field_2:) }
+  x.report("initialize init_kwarg") { InitKwarg::BenchMessage.new(field_1:, field_2:) }
+  x.report("initialize use setters") { UseSetters::BenchMessage.new(field_1:, field_2:) }
+  x.report("initialize positional") { Positional::BenchMessage.new(field_1, field_2) }
 end
-
-# 1_000_000.times do
-#   build_message
-# end
